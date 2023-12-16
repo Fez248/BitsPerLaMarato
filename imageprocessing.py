b4607ff9969c753c055dfbca4d12234a9be53d10
lmp315 <laiamogasp@gmail.com>
2023-12-16 11:59:06 +0100

    esquema bàsic

---
+++ imageprocessing.py
@@ -0,0 +1,31 @@
+
+'''
+Given an image and its overlay, it 
+
+'''
+CATEGORIES = ["light", "moderate", "saturated"]
+
+def count_bloody_pixels(image, overlay) -> int:
+    '''Returns the number of bloody pixels inside the overlay'''
+    pass
+
+def count_pixels_overlay(overlay) -> int:
+    '''Returns the total number of pixels inside the overlay'''
+    pass
+
+def get_category(image, overlay) -> str:
+    '''Given an image of a pad/tampon and its overlay, it returns
+    the category of the bleeding'''
+
+    ratio = count_bloody_pixels(image, overlay) / count_pixels_overlay(overlay)
+
+    if 0 <= ratio <= 0.33:
+        return CATEGORIES[0]
+    
+    elif 0.33 < ratio <= 0.66:
+        return CATEGORIES[1]
+    
+    else:
+        return CATEGORIES[2]
+    
+    
