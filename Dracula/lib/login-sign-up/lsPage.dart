0c6128b48ff6fde5076feef76c55d4be1a6aed66
Fez248 <ferran.beneitez@estudiantat.upc.edu>
2023-12-16 19:56:18 +0100

    firebase connection established

--- Dracula/lib/login-sign-up/lsPage.dart
+++ Dracula/lib/login-sign-up/lsPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class loginSignUp extends StatefulWidget {
     const loginSignUp({super.key, required this.title}); //constructora
@@ -46,6 +48,68 @@ class _loginSignUp extends State<loginSignUp> {
         );
     }
 }
+
+//---------------SIGN IN GOOGLE--------------------
+
+class SignInDemo extends StatefulWidget {
+  @override
+  State createState() => SignInDemoState();
+}
+
+class SignInDemoState extends State<SignInDemo> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  Future<UserCredential?> _handleSignIn() async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount!.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      final UserCredential authResult = await _auth.signInWithCredential(credential);
+      final User? user = authResult.user;
+
+      print("Signed in with Google: ${user!.displayName}");
+
+      return authResult;
+    } catch (error) {
+      print("Error signing in with Google: $error");
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Sign-In Demo'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await _handleSignIn();
+          },
+          child: Text('Sign in with Google'),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 //----------------DELETE ALL CODE BELOW THIS--------------------
--- Dracula/lib/main.dart
+++ Dracula/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import '/login-sign-up/lsPage.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); 
+  print("aaaaaaaaaaaa");
   runApp(const MyApp());
 }
 
@@ -32,7 +39,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const loginSignUp(title: 'Login or Sign Up'),
+      home: SignInDemo(),
+      //home: const loginSignUp(title: 'Login or Sign Up'),
     );
   }
 }
