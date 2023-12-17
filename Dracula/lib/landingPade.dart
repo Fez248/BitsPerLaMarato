4f3f46f69760811598f516032a057a9d518559a9
Marnark013 <marcolleo213@gmail.com>
2023-12-17 12:00:31 +0100

    CALENDAR

--- Dracula/lib/landingPade.dart
+++ Dracula/lib/landingPade.dart
@@ -6,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'main.dart';
 import 'package:camera/camera.dart';
 import 'cameraControl.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class UserProfileWelcome extends StatelessWidget {
   final User user;
@@ -16,33 +17,31 @@ class UserProfileWelcome extends StatelessWidget {
 
   
 Widget build(BuildContext context) {
-    String displayName = user.displayName ?? "DefaultName";
-    return  Scaffold(
-      appBar:  AppBar(
-        title:  Text(displayName),
-      ),
-      body:  Center(
-        child:  Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-             Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton:  FloatingActionButton(
-        onPressed: () {
-            Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => TakePictureScreen(camera: cameras.first),
-            ),
-          );
-        },
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ),
-    );
-  }
+  String displayName = user.displayName ?? "DefaultName";
+  return Scaffold(
+    appBar: AppBar(
+      title: Text(displayName),
+      backgroundColor: Colors.red, // Set the background color to red
+    ),
+    body: SfCalendar(
+      view: CalendarView.month,
+      firstDayOfWeek: 1, // Monday
+      todayHighlightColor: Colors.red,
+      backgroundColor: Colors.pink[50],
+      monthViewSettings: MonthViewSettings(showAgenda: true),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => TakePictureScreen(camera: cameras.first),
+          ),
+        );
+      },
+      tooltip: 'Increment',
+      child: Icon(Icons.add),
+    ),
+  );
+}
 }
